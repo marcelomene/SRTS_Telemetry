6b5400d19c9f5a3b604e668ebb974b48c59e3310
marcelomene <marcelo.jorda@hotmail.com>
2025-02-03 17:44:54 -0300

    Android compatibility

--- SRTS_Telemetry/Pages/HudPage.xaml.cs
+++ SRTS_Telemetry/Pages/HudPage.xaml.cs
@@ -10,5 +10,34 @@ public partial class HudPage : ContentPage
 		InitializeComponent();
 		BindingContext = App.ViewModel;
 		App.ViewModel.SetControls(this.throttleBoxview, this.brakeBoxview);
+        DeviceDisplay.Current.MainDisplayInfoChanged += Current_MainDisplayInfoChanged;
 	}
+
+    private void Current_MainDisplayInfoChanged(object? sender, DisplayInfoChangedEventArgs e)
+    {
+        var displayInfo = e.DisplayInfo;
+
+        if (displayInfo.Orientation == DisplayOrientation.Landscape)
+        {
+            Shell.SetTabBarIsVisible(this, false);
+            Shell.SetNavBarIsVisible(this, false);
+
+            foreach (var item in MainGrid.RowDefinitions)
+            {
+                if(item != null)
+                    item.Height = GridLength.Auto;
+            }
+        }
+        else if (displayInfo.Orientation == DisplayOrientation.Portrait)
+        {
+            Shell.SetTabBarIsVisible(this, true);
+            Shell.SetNavBarIsVisible(this, true);
+
+            foreach (var item in MainGrid.RowDefinitions)
+            {
+                if(item != null)
+                    item.Height = GridLength.Star;
+            }
+        }
+    }
 }
